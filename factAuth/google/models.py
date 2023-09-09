5c8cbdbd0988f69f367779ce0b4edf37ddeb67d9
Toshith <45565316+toshithh@users.noreply.github.com>
2023-09-09 22:50:25 +0530

    Delete factAuth directory

--- factAuth/google/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class GoogleAccounts(models.Model):
-    username = models.CharField(max_length=300)
-    password = models.CharField(max_length=300)
--- factAuth/google/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, re_path
-from . import views
-
-urlpatterns = [
-    path("", views.index, name="index"),
-    re_path(r'^oauth2/v2/auth/identifier_id=(?P<random_url>[-\w]+)/$', views.login, name='login'),
-    re_path(r"^signin/v2/challenge/dp-TL=&CheckConnection=(?P<random_url>[-\w]+)/$", views.v2sign, name='2-factor')
-]
